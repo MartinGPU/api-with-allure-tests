80c43b9c653d5d7db9d78a8dd8a883b7fb415005
MartinGPU <maratussr@gmail.com>
2023-05-06 22:56:21 +0400

    added allure_id change

--- src/test/java/com/marat/test/GetDomainTest.java
+++ src/test/java/com/marat/test/GetDomainTest.java
@@ -1,6 +1,10 @@
 package com.marat.test;
 
+import io.qameta.allure.AllureId;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Owner;
 import io.restassured.http.ContentType;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.marat.filters.CustomLogFilter.customLogFilter;
@@ -8,8 +12,12 @@ import static io.restassured.RestAssured.given;
 import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 import static org.hamcrest.Matchers.is;
 
+@Feature("Request to get domain")
 public class GetDomainTest {
 
+    @DisplayName("Get domain")
+    @AllureId("18463")
+    @Owner(value="Marat")
     @Test
     void getDomainRequest() {
         given()
